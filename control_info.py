a65510c18b08124137df3c469409285236f8fc00
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-08-23 14:34:23 -0700

    updated control_info to use a config file (.ini), to allow it to be used more easily

--- control_info.py
+++ control_info.py
@@ -3,6 +3,7 @@ from customtkinter import *
 import sys
 import subprocess
 import frida
+import configparser
 
 #Notes: it currently needs to be ran from the folder where mupen64-ui-console.exe is, so that it can get the proper plugins for stuff like video and sound etc.
 #Notes: this can be changed by adding command line arguments. #TODO
@@ -15,11 +16,18 @@ import frida
 #Do something with the SQL data here
 #Add customization for where to record the control data
 
+config_file = "control_info.ini"
 
 class App(CTk):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.config_parser = configparser.ConfigParser()
+        self.config_parser.read(config_file)
+        print(self.config_parser["DEFAULT"])
+        
+        for key in self.config_parser["DEFAULT"]:
+            print(key)
 
         self.main_frame = CTkFrame(self)
         self.main_frame.grid(row=0, column=0, padx=10, pady=10)
@@ -37,6 +45,11 @@ class App(CTk):
                                     command=self.select_game)
         self.game_button.grid(row=1, column=1,pady=(0, 20))
         
+        self.plugin_button = CTkButton(master=self.main_frame,
+                                    text="Select the directory path",
+                                    command=self.select_plugin)
+        self.plugin_button.grid(row=1, column=2,pady=(0, 20))
+        
         self.info_text = CTkTextbox(self.main_frame)
         self.info_text.grid(row=2, column=0,pady=(0, 20))
 
@@ -46,26 +59,31 @@ class App(CTk):
         self.exe_run_button.grid(row=3, column=0,pady=(0, 20))
 
         self.record_button =CTkButton(master=self.main_frame,
-                                    text="Record",
-                                    command=self.record_function)
+                                    text="Start Recording",
+                                    command=self.record_function,
+                                    state="disabled")
         self.record_button.grid(row=3, column=1,pady=(0, 20))
 
-        self.okButton = CTkButton(master=self.main_frame, text="Ok", command=self.ok_function)
+        self.okButton = CTkButton(master=self.main_frame, text="Quit", command=self.ok_function)
         self.okButton.grid(row=4, column=0, pady=(0, 20))
         self.main_frame.pack()
         #self.main_frame.pack_propagate(True)
         #self.geometry(f"{self.main_frame.winfo_width()}x{self.main_frame.winfo_height()}")
 
-        self.exe_name = "Not Selected"
-        self.game_name = "Not Selected"
+        self.plugin_path = self.config_parser["DEFAULT"]["mupen64plus-plugins"]
+        self.exe_name = self.config_parser["DEFAULT"]["mupen64plus-ui-console"]
+        self.game_name = self.config_parser["DEFAULT"]["n64-game"]
+        self.update_info_text()
 
         self.recording = False
         self.session = None
         self.script = None
 
     def _setup_frida(self):
+        script_file = self.config_parser["DEFAULT"]["control-script"]
+        print(script_file)
         self.session = frida.attach("mupen64plus-ui-console.exe")
-        f = open("Control Info/control_info.js", "r")
+        f = open(script_file, "r")
         self.script = self.session.create_script(
             f.read()
         )
@@ -96,9 +114,16 @@ class App(CTk):
         )
 
         self.game_name = filedialog.askopenfilename(
-            title="Select an N64 game",
+            title="Select a N64 game",
             initialdir="./",
             filetypes=filetypes
+        )
+        self.update_info_text()
+
+    def select_plugin(self):
+        self.plugin_path = filedialog.askdirectory(
+            title="Select the plugin path",
+            initialdir="./"
         )
         self.update_info_text()
 
@@ -106,13 +131,20 @@ class App(CTk):
         #maybe a better way to do this
         exe = self.exe_name
         game = self.game_name
+        plugin = self.plugin_path
         self.info_text.delete("0.0", "end")
-        self.info_text.insert("0.0", f"mupen64plus-ui-console path: {exe}\ngame path: {game}")
+        self.info_text.insert("0.0", f"plugin-path: {plugin}\nui-console path: {exe}\ngame path: {game}")
 
     def run_mupen64plus(self):
         #should run mupen64plus-ui-console with the specified game name
-        subprocess.Popen([self.exe_name, self.game_name])
+        subprocess.Popen([self.exe_name, 
+                          "--plugin", self.plugin_path,
+                          "--configdir", self.plugin_path, 
+                          "--datadir", self.plugin_path, 
+                          "--plugindir", self.plugin_path,
+                          self.game_name, ])
         self._setup_frida()
+        self.record_button.configure(state="normal")
     
     def record_function(self):
         
@@ -127,12 +159,29 @@ class App(CTk):
             self.recording = False
             self.script.unload()
             print("Stopped Recording")
+            self.record_button.configure(text="Start Recording")
         #start the recording
         else:
             self.script.load()
             self.recording = True
             print("Started Recording")
+            self.record_button.configure(text="Stop Recording")
         pass
+
+    def save_config(self):
+        self.config_parser["DEFAULT"]["mupen64plus-plugins"] = self.plugin_path
+        self.config_parser["DEFAULT"]["mupen64plus-ui-console"] = self.exe_name
+        self.config_parser["DEFAULT"]["n64-game"] = self.game_name
+
+        with open(config_file, "w") as configfile:
+            self.config_parser.write(configfile)
+            configfile.close()
+            
+
+    def quit(self) -> None:
+        #save the config
+        self.save_config()
+        return super().quit()
 
 if __name__ == "__main__":
     app = App()
